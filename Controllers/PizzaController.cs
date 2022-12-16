0567fe087669593b1ef6cdba5ce360bacdc7e334
Osama Adam <osamaadamme@gmail.com>
2022-12-17 00:11:48 +0200

    initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,52 @@
+using PizzaApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using PizzaApi.Services;
+
+namespace PizzaApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase {
+    public PizzaController() { }
+
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Pizza?> Get(int id) {
+        var pizza = PizzaService.Get(id);
+        return pizza is null ? NotFound() : pizza;
+    }
+
+    [HttpPost]
+    public IActionResult Create(Pizza pizza) {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult Update(int id, Pizza pizza) {
+        if (id != pizza.Id) {
+            return BadRequest();
+        }
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null) {
+            return NotFound();
+        }
+
+        PizzaService.Update(pizza);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id) {
+        var pizza = PizzaService.Get(id);
+        if (pizza is null) {
+            return NotFound();
+        }
+
+        PizzaService.Delete(id);
+        return NoContent();
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,7 @@
+namespace PizzaApi.Models;
+
+public class Pizza {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool GlutenFree { get; set; }
+}
